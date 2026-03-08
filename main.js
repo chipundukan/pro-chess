c758bf9603f6dd6724319686285f7cab505d6e8a
chipundukan <chipundukan@gmail.com>
2026-03-08 14:05:09 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,51 +1,13 @@
-var board = null
-var game = new Chess()
+const config = {
+  type: Phaser.AUTO,
+  width: window.innerWidth,
+  height: window.innerHeight,
+  parent: "game",
+  physics: {
+    default: "arcade",
+    arcade: { debug: false }
+  },
+  scene: [MatchScene]
+};
 
-function onDragStart(source, piece) {
-  if (game.game_over()) return false
-  if (piece.search(/^b/) !== -1) return false
-}
-
-function onDrop(source, target) {
-
-  var move = game.move({
-    from: source,
-    to: target,
-    promotion: "q"
-  })
-
-  if (move === null) return "snapback"
-
-  updateHistory()
-}
-
-function onSnapEnd() {
-  board.position(game.fen())
-}
-
-function updateHistory() {
-  document.getElementById("history").innerHTML = game.history().join("<br>")
-}
-
-function undoMove() {
-  game.undo()
-  game.undo()
-  board.position(game.fen())
-  updateHistory()
-}
-
-function resetGame() {
-  game.reset()
-  board.start()
-  document.getElementById("history").innerHTML = ""
-}
-
-window.onload = function () {
-  board = Chessboard('board', {
-    draggable: true,
-    position: 'start',
-    onDragStart: onDragStart,
-    onDrop: onDrop,
-    onSnapEnd: onSnapEnd
-  })
-}
+const game = new Phaser.Game(config);
